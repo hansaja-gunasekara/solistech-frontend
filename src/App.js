622b1c5d19f786d7d7026bd0773e4abf67121b22
hansaja-gunasekara <hgmgunasekara@gmail.com>
2024-02-29 14:30:28 +0530

    Add: Navbar component to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
+  );
 }
 
 export default App;
