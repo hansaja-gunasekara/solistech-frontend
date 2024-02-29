a08b17990129e005eeba0816fbe219159c8fa14f
hansaja-gunasekara <hgmgunasekara@gmail.com>
2024-02-29 14:34:10 +0530

    Add: routes and home page component to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </BrowserRouter>
   );
 }
